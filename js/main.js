6783de2de4ee223d29ee564ec13dcf35700b43e2
groudfrank <groudfrank@gmail.com>
2018-09-21 16:37:10 -0400

    Made a huge change to the functioning of the navigation bar, which meant that I also had to make small changes to the structure of the HTML and the JS file. The page content now sets in a div tag that is called .pg-content. The .pseudo-body is now the direct parent of .pg-content and has the brand colors gradient on it, instead of the body. The header and footer were also placed inside .puesdo-body. The navigation is now hidden behind the .pseudo-body and is partially revealed when the navigation button is click. It works well but there is more tweaking to be done - it has to be perfect of course

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
 
     $pseudo_body = $('#pseudo-body');
-    $nav_btn = $('.nav-btn');
-    $nav_dropdown_desktop = $('.nav-dropdown-desktop');
-    $nav_btn_m = $('.nav-btn-m');
+    $nav_btn = $('.nav-btn-d');
+    $desktop_nav = $('#desktop-nav');
+    $nav_bar_content = $('.navbar-content');
+    $nav_btn_m = $('.nav-btn-d-m');
     $mobile_nav = $('#mobile-nav');
+    $window_width = $(window).width();
 
     function closeNav(){
         setTimeout(function(){
-            $nav_btn.removeClass('nav-btn-transition');
+            $nav_btn.removeClass('nav-btn-d-transition');
         }, 1000);
-        TweenMax.to($nav_dropdown_desktop, 0.2, {y: -100, opacity: 1, delay:1, onComplete: navbarOrigin});
+        // TweenMax.to($nav_dropdown_desktop, 0.2, {y: -100, opacity: 1, delay:1, onComplete: navbarOrigin});
+        pseudoBodyOrigin();
 
     };
 
@@ -20,9 +23,9 @@ $(document).ready(function(){
     }
 
     $pseudo_body.on('click', function(){
-        if($nav_dropdown_desktop.hasClass('hide-me') == false){
-            $nav_btn.removeClass('nav-btn-transition');
-            TweenMax.to($nav_dropdown_desktop, 0.2, {y: -100, opacity: 1, onComplete: navbarOrigin});
+        if($desktop_nav.hasClass('navigation-closed') == false){
+            $nav_btn.removeClass('nav-btn-d-transition');
+            pseudoBodyOrigin();
         }
     })
 
@@ -43,21 +46,24 @@ $(document).ready(function(){
             } 
           });
 
-    // Returns navbar to origginal position
-    var navbarOrigin = function(){
-        $nav_dropdown_desktop.addClass('hide-me');
-        TweenMax.to($nav_dropdown_desktop, 0, {y: 0});
+    // Returns navbar to original position
+    var pseudoBodyOrigin = function(){
+        $desktop_nav.addClass('navigation-closed');
+        TweenMax.to($pseudo_body, 0.2, {x: 0});
     }
 
+    // On navigation button click(if class is hidden move the )
+    // navigation bar into view and change the color of the navigation bar
+    // icon.
     $nav_btn.on('click', function(){
-        if($nav_dropdown_desktop.hasClass('hide-me')){
-            $nav_dropdown_desktop.removeClass('hide-me');
-            $nav_btn.addClass('nav-btn-transition');
-            TweenMax.from($nav_dropdown_desktop, 0.2, {y: -100, opacity: 0});
+        if($desktop_nav.hasClass('navigation-closed')){
+            $desktop_nav.removeClass('navigation-closed');
+            $nav_btn.addClass('nav-btn-d-transition');
+            TweenMax.to($pseudo_body, 0.2, {x: ($window_width * 0.25)});
         }
         else{
-            $nav_btn.removeClass('nav-btn-transition');
-            TweenMax.to($nav_dropdown_desktop, 0.2, {y: -100, opacity: 1, onComplete: navbarOrigin});
+            $nav_btn.removeClass('nav-btn-d-transition');
+            pseudoBodyOrigin();
         }
     });
 
@@ -65,12 +71,14 @@ $(document).ready(function(){
         $mobile_nav.addClass('no-display');
     }
 
-     // Returns navbar to origginal position
+     // Returns navbar to original position
      var navbarOriginMobile = function(){
         $mobile_nav.addClass('no-display');
         TweenMax.to($mobile_nav, 0, {opacity: 1, scale: 1});
     }
 
+
+    // Mobile navigation bar transition.
     $nav_btn_m.on('click', function(){
         if($mobile_nav.hasClass('no-display')){
             $mobile_nav.removeClass('no-display');
